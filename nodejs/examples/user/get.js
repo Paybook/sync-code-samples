503d21b66549db62c68b967e180f72bbcc932d4f
JGuardianM <81170597+JGuardianM@users.noreply.github.com>
2025-03-14 09:49:09 -0600

    Update get.js
    Update URL

--- nodejs/examples/user/get.js
+++ nodejs/examples/user/get.js
@@ -5,7 +5,7 @@ const args = require('minimist')(process.argv.slice(2))
 
 const SYNC_API_KEY = args['api_key'];
 
-const SYNC_API = 'https://sync.paybook.com/v1';
+const SYNC_API = 'https://opendata-api.syncfy.com/v1';
 
 async function main(){
   try{
