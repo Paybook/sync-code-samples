43f1fb22f1b05f4db5d43f9073fcbaa1c6779352
JGuardianM <81170597+JGuardianM@users.noreply.github.com>
2025-03-14 09:48:02 -0600

    Update delete.js
    Update URL

--- nodejs/examples/user/delete.js
+++ nodejs/examples/user/delete.js
@@ -6,7 +6,7 @@ const args = require('minimist')(process.argv.slice(2))
 const SYNC_API_KEY = args['api_key'];
 const SYNC_ID_USER = args['id_user'];
 
-const SYNC_API = 'https://sync.paybook.com/v1';
+const SYNC_API = 'https://opendata-api.syncfy.com/v1';
 
 async function main(){
   try{
